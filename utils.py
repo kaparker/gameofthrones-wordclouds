2e218f7a0122e07a2fc5e19cacd21a8583313e02
Kerry Parker <kerry.parker@deliveroo.co.uk>
2022-11-02 11:26:58 +0000

    Move code into notebook

---
+++ utils.py
@@ -0,0 +1,48 @@
+import re
+import unicodedata
+import nltk
+from nltk.corpus import stopwords
+from contractions import CONTRACTION_MAP
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+
+def removetitle(text):
+    return re.sub(r'.*:', '', text)
+
+def removebrackets(text):
+    return re.sub('[\(\[].*?[\)\]]', ' ', text)
+
+def remove_accented_chars(text):
+    return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+
+def remove_special_chars(text, remove_digits=False):
+    pattern = r'[^a-zA-Z0-9\s]' if not remove_digits else r'[^a-zA-Z\s]'
+    return re.sub(pattern, '', text)
+
+def remove_stopwords(text):
+    nltk.download('punkt')
+    nltk.download('stopwords')
+    stopword_list = stopwords.words('english')
+    tokens = nltk.word_tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    return ' '.join([token for token in tokens if token not in stopword_list])
+
+def lemmatize(text):
+    text = nlp(text)
+    return ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+
+def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+                                      flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match)\
+                                if contraction_mapping.get(match)\
+                                else contraction_mapping.get(match.lower())
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    return re.sub("'", "", expanded_text)
